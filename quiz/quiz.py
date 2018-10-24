228584959d31496b6b1a61ffd605e46b7b55c39b
Deirdre Weldon <deirdreweldon18@gmail.com>
2018-10-24 12:10:41 +0000

    modified questions.txt, quiz.py - updating code to keep score of questions asked and answered

--- quiz/quiz.py
+++ quiz/quiz.py
@@ -16,8 +16,8 @@ def show_menu():
 #The last part of our game that we need to write is the actual quiz itself, and we're going to do this incrementally. Maybe we'll start by loading the questions from the file and asking them but ignoring the answers. Then, a little bit later, we'll add answer checking and score-keeping. Let's go ahead and add our ask_questions() function. We'll begin by creating two empty lists, one for the questions and another one for the answers.     
 
 def ask_questions():
-    questions = ["What is capital of France?", "What is capital of Ireland?"]
-    answers = ["Paris", "Dublin"]
+    questions = []
+    answers = []
     
 #Now, we're going to use a slightly different method of opening our file this time. Instead of the method that we've learnt already, we're going to use a with block. There are many reasons for using this, which are good in general development. As you can see, the syntax is very similar. We're opening it with the 'read' access mode flag as file. The handy thing about the with block is that it handles a lot of stuff for us. For instance, we don't need to worry about closing the file because at the end of the width block it will be closed.    
 
@@ -32,14 +32,37 @@ def ask_questions():
                 
         else:
             answers.append(text)
-                
-    for question, answer in zip(questions, answers):
-        guess = input(question + "> ")
-            
+    
 
+#et's go ahead now and add in the functionality to check the answers and also to keep score. So, here now we need to know the number of questions that there are in our file. We do that by checking the length of our questions list.
+    number_of_questions = len(questions)
     
+    questions_and_answers = zip(questions, answers)
     
+    #  And, finally, we'll initialize an empty variable called score, which at the beginning, of course, is set to zero. 
     
+    score = 0
+    
+    for question, answer in (questions_and_answers):
+    #for question, answer in zip(questions, answers):
+        guess = input(question + "> ")
+        
+    #Now, instead of the zip function, we go through our questions and answers list. Our guess can stay the same, but now we need to check to make sure that the answer is correct. So, if our guess is equal to our answer then it's correct and we add 1 to the score. We then print that we were right, and we'll also then print the score as well just so we can keep track of what's going on. If we get the answer wrong then we're just going to print 'wrong'.  
+    
+    if guess == answer:
+        score += 1
+        print ("right!")
+        print (score)
+    else:
+        print ("wrong!")
+       
+   #And, finally, when the whole loop has run round and we've gone through every single one of our questions and answers, then we can print that you got so many correct (spell 'out' correctly) out of so many, and we'll use our format method again. This time, we pass in our score and the total number of questions. So, it will say you got so many correct out of the total number of questions. So, this makes sense. You can see that we're incrementing the score. We can see that we've got a right and a wrong message. Let's save it. We'll run it again and check that it works. python3 quiz.py Option 1. What is the capital of Ireland? Dublin. It says that's right, 1, and we got 1 correct out of 1. So, let's just add another question. What is the capital of France? The answer is Paris and when we ask now, we get our first question up. Dublin, which is right. Let's try giving it a wrong answer. See what happens? We get 'wrong', and then it tells us that we've got one correct out of two. That concludes our lesson on Python file handling. We've looked at two different methods for opening files, and we've examined the various different file access modes that are available to us in Python. We've also looked at the different functions that can enumerate and join together lists. So, before moving on maybe think about ways that you could improve this game - perhaps by random question choice or even the ability to delete a question. File handling is an important part of any programming language, so you've come a long way here with Python. Don't forget to add and commit your code to GitHub before moving on to the next lesson.   
+   
+    print ("You got {0} correct out of {1}".format(score, number_of_questions))
+        
+#Then, we're going to use questions and answers here as another variable and we're going to call the zip function on this. This prevents us having to run the zip function every time the for loop is run.        
+   
+    #questions_and_answers = zip(questions, answers)
     
     
 #So, now that we have that working let's actually add the option to add a question to our file. We want to be prompted for a question and answer, and then we want both of them to be appended to the questions.txt file. So, let's create a new function. We'll add it just below our menu function, and we'll call it add_question().  So, first of all, let's print a blank line again and then our question prompt. We're going to store it in a question variable. Again, we'll use input and our prompt will be: 'Enter a question'. We'll put a new line here, and then a greater-than sign as a prompt, and a space just to make it look nice.
@@ -67,9 +90,7 @@ def game_loop():
     #just another way to say loop forever.
     while True:
         
-        #Let's just add in the functionality to actually get it working from our menu. We put the ask_questions() function there, and then talk for a minute about exactly what's going on here. So, as we said, we use the with block; we open the questions.txt; we use the enumerate function, which creates a tuple in memory with a line number at the beginning, which we represent with 'i' and then the text. We check to see whether the line numbers are even or odd and we put them into questions or answers, and then, finally, we use a zip function to put everything together into another tuple in memory, and this looks like this. This is what we're left with - a tuple that contains a question and our answer, a question and our answer. So, we can easily check the answer from our question because the two are related. They're effectively two columns of text. Let's just check to see that this works then. 
-        
-        
+#Let's just add in the functionality to actually get it working from our menu. We put the ask_questions() function there, and then talk for a minute about exactly what's going on here. So, as we said, we use the with block; we open the questions.txt; we use the enumerate function, which creates a tuple in memory with a line number at the beginning, which we represent with 'i' and then the text. We check to see whether the line numbers are even or odd and we put them into questions or answers, and then, finally, we use a zip function to put everything together into another tuple in memory, and this looks like this. This is what we're left with - a tuple that contains a question and our answer, a question and our answer. So, we can easily check the answer from our question because the two are related. They're effectively two columns of text. Let's just check to see that this works then.  So, we'll select option 1 to ask a question: What is the capital of Ireland? The answer is Dublin. So it asks a question - ok, at the moment, it's discarding the answer - but we can see that our question asking part is working. 
         
         option = show_menu()
         if option == "1":
