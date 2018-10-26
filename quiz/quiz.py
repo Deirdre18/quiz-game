f8ea690e418c090177c402ccebd030352044798e
Deirdre Weldon <deirdreweldon18@gmail.com>
2018-10-26 01:15:41 +0000

    quiz.py modified

--- quiz/quiz.py
+++ quiz/quiz.py
@@ -43,22 +43,22 @@ def ask_questions():
     
     score = 0
     
-    for question, answer in (questions_and_answers):
+    for question, answer in questions_and_answers:
     #for question, answer in zip(questions, answers):
         guess = input(question + "> ")
         
     #Now, instead of the zip function, we go through our questions and answers list. Our guess can stay the same, but now we need to check to make sure that the answer is correct. So, if our guess is equal to our answer then it's correct and we add 1 to the score. We then print that we were right, and we'll also then print the score as well just so we can keep track of what's going on. If we get the answer wrong then we're just going to print 'wrong'.  
     
-    if guess == answer:
-        score += 1
-        print ("right!")
-        print (score)
-    else:
-        print ("wrong!")
+        if guess == answer:
+            score += 1
+            print ("right!")
+            print (score)
+        else:
+            print ("wrong!")
        
    #And, finally, when the whole loop has run round and we've gone through every single one of our questions and answers, then we can print that you got so many correct (spell 'out' correctly) out of so many, and we'll use our format method again. This time, we pass in our score and the total number of questions. So, it will say you got so many correct out of the total number of questions. So, this makes sense. You can see that we're incrementing the score. We can see that we've got a right and a wrong message. Let's save it. We'll run it again and check that it works. python3 quiz.py Option 1. What is the capital of Ireland? Dublin. It says that's right, 1, and we got 1 correct out of 1. So, let's just add another question. What is the capital of France? The answer is Paris and when we ask now, we get our first question up. Dublin, which is right. Let's try giving it a wrong answer. See what happens? We get 'wrong', and then it tells us that we've got one correct out of two. That concludes our lesson on Python file handling. We've looked at two different methods for opening files, and we've examined the various different file access modes that are available to us in Python. We've also looked at the different functions that can enumerate and join together lists. So, before moving on maybe think about ways that you could improve this game - perhaps by random question choice or even the ability to delete a question. File handling is an important part of any programming language, so you've come a long way here with Python. Don't forget to add and commit your code to GitHub before moving on to the next lesson.   
    
-    print ("You got {0} correct out of {1}".format(score, number_of_questions))
+        print ("You got {0} correct out of {1}".format(score, number_of_questions))
         
 #Then, we're going to use questions and answers here as another variable and we're going to call the zip function on this. This prevents us having to run the zip function every time the for loop is run.        
    
@@ -76,7 +76,7 @@ def add_question():
 #So again, we'll store this in the answer variable. We'll use an input. This time, we'll take the question that we'd already asked, do a blank line, a greater-than sign for a prompt and then, using the format method, we'll insert the question there. So, now the user will be asked the question again when they come to put the answer in.    
     answer = input("{0}\n> ".format(question))
     
-#ow, we get on to the part where we actually add this to the file. We'll open our questions.txt file for appending using the 'a' access mode flag. The first thing we'll do is write our question, and then we'll add a new line at the end. The second thing we'll do, then, is write our answer, and we'll add a new line at the end. So, each question and answer will go on its own line. Finally, we'll close the file. So, that function works nicely. 
+#Now, we get on to the part where we actually add this to the file. We'll open our questions.txt file for appending using the 'a' access mode flag. The first thing we'll do is write our question, and then we'll add a new line at the end. The second thing we'll do, then, is write our answer, and we'll add a new line at the end. So, each question and answer will go on its own line. Finally, we'll close the file. So, that function works nicely. 
     file = open("questions.txt", "a")
     file.write(question + "\n")
     file.write (answer + "\n")
